8a11b7bd7041b66278cc3c77d24682c3b848d097
abhijeetsuryawanshi12 <abhijeetsuryawanshi128@gmail.com>
2025-07-29 17:50:54 +0530

    rearrange(streamlit) : files deleted and unnecessary code also deleted

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhYmhpamVldHN1cnlhd2Fuc2hpMTI4QGdtYWlsLmNvbSIsImV4cCI6MTc1NDM5MTA0Mn0.bdAs68I2gt6Gogz9Rv3giABdfUindimjS3tsQ7fSavE"
-headers = {"Authorization": f"Bearer {token}"}
-response = requests.get("http://127.0.0.1:5000/profile/me", headers=headers)
-print(f"Status: {response.status_code}")
-print(f"Response: {response.text}")
